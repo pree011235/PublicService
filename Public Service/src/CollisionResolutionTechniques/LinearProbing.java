f0ce4f2c7878f64ffe242916191c16a1f5ae3208
preethi <pree011235@gmail.com>
2014-11-08 12:32:49 -0800

    Linear Probing Weekend pet project
    Implements a hash map which maps using linear probing technique

---
+++ Public Service/src/CollisionResolutionTechniques/LinearProbing.java
@@ -0,0 +1,35 @@
+package CollisionResolutionTechniques;
+
+public class LinearProbing<Key, Value> {
+	private int size = 300001;
+	private Value[] value = (Value[]) new Object[size];
+	private Key[] key = (Key[]) new Object[size];
+	private int currSize;
+
+	public int getHash(Key thiskey){
+		return key.hashCode();
+	}
+	
+	public Value put(Key thiskey, Value thisval) {
+		int hash = thiskey.hashCode();
+		for (int i = hash; key[i] != null; i = (i + 1) % size) {
+			if (getHash(key[i]) == hash)
+				break;
+			key[i] = thiskey;
+			value[i] = thisval;
+		}
+		currSize ++;
+		return thisval;
+	}
+
+	public Value get(Key thiskey) {
+		int hash = thiskey.hashCode();
+		for (int i = hash; key[i] != null; i = (i + 1) % size) {
+			if (getHash(key[i]) == hash)
+				return value[i];
+		}
+		return null;
+	}
+	
+	public 
+}
