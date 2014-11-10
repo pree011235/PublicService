5eaa6bcc557e545feee0ffbec7e2f676c9d2afff
preethi <pree011235@gmail.com>
2014-11-09 19:38:49 -0800

    Union, Intersection and Difference

--- Public Service/src/SetOperation/Set1.java
+++ Public Service/src/SetOperation/Set1.java
@@ -3,7 +3,7 @@ package SetOperation;
 import java.util.*;
 
 public class Set1 {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Set<String> a = new HashSet<String>();
 		a.add("A");
 		a.add("B");
@@ -14,12 +14,28 @@ public class Set1 {
 		b.add("A");
 		b.add("B1");
 		b.add("C1");
-		b.add("D1");
+		b.add("D");
 
 		Set<String> union = new HashSet<String>();
 		union.addAll(a);
 		union.addAll(b);
-		for(String s: union)
+//		for (String s : union)
+//			System.out.println(s);
+
+		Set<String> intersection = new HashSet<String>();
+		intersection.addAll(a);
+		intersection.retainAll(b);
+//		for (String s : intersection)
+//			System.out.println(s);
+
+		/*
+		 * Unionr minus intersections is the symmetric diff
+		 */
+		Set<String> minus = new HashSet<String>();
+		minus.addAll(union);
+		minus.removeAll(intersection);
+		for (String s : minus)
 			System.out.println(s);
+
 	}
 }
